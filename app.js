fc3a8da0bc8e4b55a8647396028fbfd8e6b0e83a
MusicMan <musicman@MacBook-Pro.fios-router.home>
2020-10-22 10:09:10 -0400

    [callbacks-timing-iterators] add 5 more functions

--- app.js
+++ app.js
@@ -2,23 +2,63 @@ function sayHello (name) {
     let msg = 'Hello, ' + name + '. How are you?';
     return msg;
 })
+console.log(sayHello('SEI 1019'));
 
 function sayHello (name) {
     let msg = 'Hello, ' + name + '. How are you?;
     return msg;'
 }
+console.log(sayHello('General Assembally!'));
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('Good Day'));
 
 function sayHello (name) {
     let msg = 'Hello, ' + name + '. How are you?';
     return msg;
 }
 
+console.log(sayHello('Time to Rise'));
 function sayHello (name) {
     let msg = 'Hello, ' + name + '. How are you?';
     return msg;
 }
 
 function sayHello (name) {
-    let msg = 'Hello, ' + name + '. How are you?';
+    let msg = 'Hello, ' + name + '.How are you?';
     return msg;
 }
+console.log(sayHello('How are you'));
+function sayHello (name) {
+    let msg = 'Hello, ' + name + '.How are you?';
+    return msg;
+}
+
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + '.How are you?';
+    return msg;
+}
+console.log(sayHello('Hi'));
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + '.How are you?';
+    return msg;
+}
+
+console.log(sayHello('Just Passing By'));
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + '.How are you?';
+    return msg;
+}
+console.log(sayHello('another adieu'));
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + '.How are you?';
+    return msg;
+}
+console.log(sayHello('and Goodbye!'));
