75bd24491f73b4dc8ebdbb0308c4ae83cb4c63a9
MusicMan <musicman@MacBook-Pro.fios-router.home>
2020-10-22 09:54:22 -0400

    [callbacks-timing-iterators] add function example

---
+++ app.js
@@ -0,0 +1,24 @@
+function sayHello (name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+})
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + '. How are you?;
+    return msg;'
+}
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+function sayHello (name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
